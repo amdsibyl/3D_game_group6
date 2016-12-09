81e1f3ae783606888385b418813dda44c1823d10
hsuannn <didi.ya35@gmail.com>
2016-12-10 00:32:54 +0800

    + EnergyManagerScript
    還沒跟觸控連動

---
+++ Project/Assets/EnergyManagerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EnergyManagerScript : MonoBehaviour {
+	public float maxEnergy;
+	public float energy;
+	public Image bar;
+	private float level;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		level = energy / maxEnergy;
+		bar.fillAmount = level * 0.5f;
+	}
+}
