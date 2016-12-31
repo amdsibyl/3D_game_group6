15d95b7cb16188d9f87491b557f83a6999e49175
Greent Wu <lynn84961@gmail.com>
2016-12-31 23:03:25 +0800

    end diamond particle change
    end diamond disappear while collided
    end particle added

--- Project/Assets/Scripts/Managers/EndCubeManager.cs
+++ Project/Assets/Scripts/Managers/EndCubeManager.cs
@@ -2,22 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EndDiamondManager : MonoBehaviour {
-	public GameObject Diamond;
-	public ParticleSystem ps;
+public class EndCubeManager : MonoBehaviour {
+	
+	public ParticleSystem particleSystem;
 	// Use this for initialization
 	void Start () {
-	//	Diamond.SetActive (true);
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-
 	void OnCollisionEnter(Collision collision){
-		ps.Play ();
-		Diamond.SetActive (false);
+		particleSystem.Play ();
 
 	}
 }
